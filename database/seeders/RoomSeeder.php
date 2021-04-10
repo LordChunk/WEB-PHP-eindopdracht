c98096d0d4aab713fcfd51e359f885773daaf603
Job <LordChunk@users.noreply.github.com>
2021-04-10 17:38:10 +0200

    Fixed room seeder

--- database/seeders/RoomSeeder.php
+++ database/seeders/RoomSeeder.php
@@ -14,28 +14,28 @@ class RoomSeeder extends Seeder {
     public function run()
     {
         Room::create([
-            'seat_column' => 6,
-            'seat_row' => 3,
+            'column' => 6,
+            'row' => 3,
             'cinema_id' => 1,
         ]);
         Room::create([
-            'seat_column' => 6,
-            'seat_row' => 4,
+            'column' => 6,
+            'row' => 4,
             'cinema_id' => 1,
         ]);
         Room::create([
-            'seat_column' => 6,
-            'seat_row' => 5,
+            'column' => 6,
+            'row' => 5,
             'cinema_id' => 1,
         ]);
         Room::create([
-            'seat_column' => 6,
-            'seat_row' => 5,
+            'column' => 6,
+            'row' => 5,
             'cinema_id' => 2,
         ]);
         Room::create([
-            'seat_column' => 6,
-            'seat_row' => 7,
+            'column' => 6,
+            'row' => 7,
             'cinema_id' => 2,
         ]);
     }
