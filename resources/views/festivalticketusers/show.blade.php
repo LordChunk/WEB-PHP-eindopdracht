8e97e65cb7338813c111ec17ebfed052b44cc00a
Job <LordChunk@users.noreply.github.com>
2021-04-11 17:25:24 +0200

    changed some form values

--- resources/views/festivalticketusers/show.blade.php
+++ resources/views/festivalticketusers/show.blade.php
@@ -3,23 +3,25 @@
     <h1 class="font-weight-bold">{{__('Selected Festival:')}} {{$festival->name}}</h1>
 @endsection
 @section('content')
-    <form action="#" method="post">
+    <form action="{{route('festivalticketusers.store')}}" method="post">
         @csrf
 
         <div>
             <label for="number_of_tickets" class="col-form-label">{{__('Number of tickets')}}:</label>
 
-            <input  type="text"
-                   class="form-control @error('name') is-invalid @enderror" name="name"
-                   value="{{ old('name') }}" autocomplete="name" autofocus>
+            <input type="number"
+                   class="form-control @error('name') is-invalid @enderror" name="number_of_tickets"
+                   value="{{ old('number_of_tickets') }}" autofocus>
 
             @error('name')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
+
+            <input type="hidden" name="festival_id" value="{{$festival->id}}">
         </div>
 
-        <button class="btn btn-primary">Maak aan</button>
+        <button class="btn btn-primary">Create</button>
     </form>
 @endsection
