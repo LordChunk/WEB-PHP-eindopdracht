46a3bc1adc44fd4f6f83fc057735d2755dfdd69d
mftver <m.vermulst@student.avans.nl>
2021-04-11 17:20:04 +0200

    reformatted file

--- resources/views/festivalticketusers/show.blade.php
+++ resources/views/festivalticketusers/show.blade.php
@@ -15,39 +15,11 @@
 
             @error('name')
             <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
+                <strong>{{ $message }}</strong>
+            </span>
             @enderror
         </div>
 
-
-        <div>
-            <label for="number_of_tickets" class="col-form-label">{{__('begin')}}:</label>
-
-            <input  type="text"
-                    class="form-control @error('name') is-invalid @enderror" name="name"
-                    value="{{ old('name') }}" autocomplete="name" autofocus>
-
-            @error('name')
-            <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-            @enderror
-        </div>
-
-        <div>
-            <label for="number_of_tickets" class="col-form-label">{{__('Number of tickets')}}:</label>
-
-            <input  type="text"
-                    class="form-control @error('name') is-invalid @enderror" name="name"
-                    value="{{ old('name') }}" autocomplete="name" autofocus>
-
-            @error('name')
-            <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-            @enderror
-        </div>
         <button class="btn btn-primary">Maak aan</button>
     </form>
 @endsection
