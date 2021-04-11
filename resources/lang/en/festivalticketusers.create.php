2370ff1b7cdfd9f388adc190758196e438f398b7
mftver <m.vermulst@student.avans.nl>
2021-04-11 23:40:58 +0200

    added resource files

---
+++ resources/lang/en/festivalticketusers.create.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'Selected Festival:' => 'Selected Festival:',
+    'Number of tickets' => 'Number of tickets',
+    'Start day' => 'Start day',
+    'End day' => 'End day',
+    'Photo' => 'Photo',
+    'Order Tickets!' => 'Order Tickets!',
+
+];
---
+++ resources/lang/nl/festivalticketusers.create.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'Selected Festival:' => 'Geselecteerde Festival:',
+    'Number of tickets' => 'Aantal tickets',
+    'Start day' => 'Start dag',
+    'End day' => 'Eind dag',
+    'Photo' => 'foto',
+    'Order Tickets!' => 'Order Tickets!',
+
+];
