282b20c4cca69690efdc1d8bb388c8266dc5b267
Job <LordChunk@users.noreply.github.com>
2021-04-11 21:03:14 +0200

    Made proper home page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('restaurants.index')" :active="request()->routeIs('restaurants.index')">
+                        {{ __('Restaurants') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('orderables.index')" :active="request()->routeIs('orderables.index')">
+                        {{ __('Festivals & Cinemas') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -79,6 +85,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('restaurants.index')" :active="request()->routeIs('orderables.index')">
+                {{ __('Restaurants') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('orderables.index')" :active="request()->routeIs('orderables.index')">
+                {{ __('Festivals & Cinemas') }}
             </x-responsive-nav-link>
         </div>
 
